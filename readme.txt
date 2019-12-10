008ddc8da295358425f005732c89ba60290a5030
liufangpu <424395537@qq.com>
2019-12-11 00:52:05 +0800

    readme

---
+++ readme.txt
@@ -0,0 +1,3 @@
+将videos目录下的实例 asss.mp4文件放入指定位置
+ WebMvcConfig中配置的映射路径中，
+ 即可通过网页访问http://localhost:8080/result
--- src/main/java/com/example/demovideo/huawei/config/WebMvcConfig.java
+++ src/main/java/com/example/demovideo/huawei/config/WebMvcConfig.java
@@ -1,12 +1,12 @@
-package com.example.demovideo.huawei.dao;
+package com.example.demovideo.huawei.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
